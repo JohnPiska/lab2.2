61d3da4150e6bab945106318a8d353ddaa4ec5b8
JohnPiska <159447800+JohnPiska@users.noreply.github.com>
2025-03-10 13:50:03 +0500

    Update Program.fs

--- lab2.2/Program.fs
+++ lab2.2/Program.fs
@@ -1,27 +1,26 @@
-﻿open System
+open System
 
 let endsWithDigit number digit =
     let lastDigit = abs number % 10  
     lastDigit = digit                
 
 let sumMatchingNumbers numbers digit =
-    List.fold (fun sum number -> 
-        if endsWithDigit number digit then sum + number
-        else sum
-    ) 0 numbers
+    numbers
+    |> Seq.filter (fun number -> endsWithDigit number digit)  
+    |> Seq.sum 
 
 let rec getNumbersFromUser () =
     printf "Введите числа через пробел: "
     let input = Console.ReadLine()
     
     try
-        let numberStrings = input.Split(' ')  
-        let numbers = List.map int (Array.toList numberStrings) 
-        numbers  
+        input.Split(' ')  
+        |> Seq.map int   
+        |> Seq.toList   
     with
-        | :? FormatException -> 
-            printfn "Ошибка: Введите только числа!"
-            getNumbersFromUser () 
+    | :? FormatException ->  
+        printfn "Ошибка: Введите только числа!"
+        getNumbersFromUser () 
 
 let rec getDigitFromUser () =
     printf "Введите одну цифру (0-9): "
@@ -33,10 +32,11 @@ let rec getDigitFromUser () =
         printfn "Ошибка: Введите одну цифру!"
         getDigitFromUser ()
 
-let numbers = getNumbersFromUser () 
-let digit = getDigitFromUser ()     
+let numbers = getNumbersFromUser ()  
+let digit = getDigitFromUser ()      
 
-let sum = sumMatchingNumbers numbers digit 
+let sum = sumMatchingNumbers numbers digit  
+
 
 printfn "Исходный список чисел: %A" numbers
 printfn "Сумма чисел, оканчивающихся на %d: %d" digit sum
